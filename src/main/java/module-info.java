480b6a1448a3cc6567b6787fdf115da9de0623b7
Noorani Bakerally <noorani.bakerally@gmail.com>
2024-05-21 11:50:55 +0400

    new modifs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.intelidia.testquerydsl {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.persistence;
+    requires com.querydsl.core;
+    requires java.compiler;
 
 
     opens com.intelidia.testquerydsl to javafx.fxml;
